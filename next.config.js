8b00abba737642ff2acccbaa5ea62d4a07de3d43
Jiachi Liu <inbox@huozhi.im>
2022-03-25 09:40:14 +0100

    remove locale: false

--- next.config.js
+++ next.config.js
@@ -9,13 +9,11 @@ module.exports = {
         {
           destination: '/en/betway.com',
           has: [{ key: 'x-bwg-host', type: 'header', value: 'betway.com' }],
-          locale: false,
           source: '/en/',
         },
         {
           destination: '/de/betway.com',
           has: [{ key: 'x-bwg-host', type: 'header', value: 'betway.com' }],
-          locale: false,
           source: '/de/',
         },
       ]
