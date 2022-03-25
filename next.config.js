85a9008c0cfd13a3d0643e690f9c40f2cd4458ad
Jiachi Liu <inbox@huozhi.im>
2022-03-25 22:15:16 +0100

    rewrite locales

--- next.config.js
+++ next.config.js
@@ -1,25 +1,24 @@
 module.exports = {
   i18n: {
-    locales: ['nl', 'fr', 'de',],
+    locales: ['nl', 'fr', 'de'],
     defaultLocale: 'de',
   },
   async rewrites() {
     return {
       fallback: [
         {
-          source: '/',
+          source: '/de',
           destination: '/de/betway.com',
+          locale: false,
+        },
+        {
+          source: '/fr',
+          destination: '/fr/betway.com',
           locale: false,
         },
         {
           source: '/nl',
-          destination: '/de/betway.com',
-          locale: false,
-        },
-        {
-          source: '/zh-CN',
-          destination: '/zh-CN/betway.com',
-          has: [{ key: 'x-bwg-host', type: 'header', value: 'betway.com' }],
+          destination: '/nl/betway.com',
           locale: false,
         },
         {
@@ -28,11 +27,6 @@ module.exports = {
           has: [{ key: 'x-bwg-host', type: 'header', value: 'betway.com' }],
           locale: false,
         }, 
-        {
-          source: '/',
-          destination: '/de/betway.com',
-          locale: false,
-        },
       ],
     }
   },
