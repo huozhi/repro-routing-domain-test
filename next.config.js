c1a133cc7d4b44936bfcd585de37b656caf6ca23
Jiachi Liu <inbox@huozhi.im>
2022-03-25 22:05:12 +0100

    update next conf

--- next.config.js
+++ next.config.js
@@ -1,37 +1,39 @@
 module.exports = {
   i18n: {
-    locales: ['nl', 'fr', 'de', 'zh-CN'],
-    defaultLocale: 'fr',
+    locales: ['nl', 'fr', 'de',],
+    defaultLocale: 'de',
   },
   async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/betway.com',
-        // locale: false,
-      },
-      {
-        source: '/zh-CN',
-        destination: '/zh-CN/betway.com',
-        locale: false,
-      },
-      {
-        source: '/nl',
-        destination: '/de/betway.com',
-        locale: false,
-      },
-      {
-        source: '/zh-CN',
-        destination: '/zh-CN/betway.com',
-        has: [{ key: 'x-bwg-host', type: 'header', value: 'betway.com' }],
-        locale: false,
-      },
-      {
-        source: '/de',
-        destination: '/de/betway.com',
-        has: [{ key: 'x-bwg-host', type: 'header', value: 'betway.com' }],
-        locale: false,
-      },
-    ]
+    return {
+      fallback: [
+        {
+          source: '/',
+          destination: '/de/betway.com',
+          locale: false,
+        },
+        {
+          source: '/nl',
+          destination: '/de/betway.com',
+          locale: false,
+        },
+        {
+          source: '/zh-CN',
+          destination: '/zh-CN/betway.com',
+          has: [{ key: 'x-bwg-host', type: 'header', value: 'betway.com' }],
+          locale: false,
+        },
+        {
+          source: '/de',
+          destination: '/de/betway.com',
+          has: [{ key: 'x-bwg-host', type: 'header', value: 'betway.com' }],
+          locale: false,
+        }, 
+        {
+          source: '/',
+          destination: '/de/betway.com',
+          locale: false,
+        },
+      ],
+    }
   },
 }
